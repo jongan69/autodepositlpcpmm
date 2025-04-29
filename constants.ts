e857859982827210f44f736b7eacef93e385d896
Jonathan Gan <jonathang132298@gmail.com>
2025-04-29 04:30:42 -0400

    Tetsting

---
+++ constants.ts
@@ -0,0 +1,14 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const M3_POOL_ADDRESSES = [
+  "B9H5YkVyPQChK62rqGTSCZRfGFXaw91GRjJpwkc8R7XA",
+  "7VrKfJZ1D7B9ydmd8HXS76vX47A3rzKPDCok82SvFvRV",
+  "FiLQByi38ncaQMMVncWSttSQ9GSRFexP3vZuvgrBY5AP",
+];
+
+export const DLLM_POOL_ADDRESS = "H3fHUEVRZ2iWuCS1tmRg9e4x76QtcBASxniYBzkGYbZr";
+
+export const RAYDIUM_POOL_ADDRESSES = [
+  "G2MRSjNjCbFUmMf32Z1aXYhy6pc1ihLyYg6orKedyjJG",
+  "G2MRSjNjCbFUmMf32Z1aXYhy6pc1ihLyYg6orKedyjJG",
+];
---
+++ index.ts
@@ -0,0 +1,5 @@
+// import { addLiquidity } from "./raydium/addlp/amm/addliquidityamm";
+import { deposit } from "./raydium/cpmm/depositliquiditycpmm";
+
+// Deposit liquidity to cpmm pool
+deposit()
